263d715578e40cc7169a5ed512edac6e11d63ed4
Strategy <Dima.kogtev.95@mail.ru>
2023-11-29 03:26:21 +0300

    last commit

--- src/Generation.java
+++ src/Generation.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Generation {
-}
+public class Generation {
+
+    public void GenerationPass (String pass) {
+        String passOld = pass;
+        int key = 1410;
+        System.out.println(passOld);
+        char[] textArray = passOld.toCharArray();
+        for (int i = 0; i < textArray.length; i++) {
+            textArray[i] ^= key;
+        }
+        passOld = new String(textArray);
+        System.out.println(passOld);
+        for (int i = 0; i < passOld.length(); i++) {
+            textArray[i] ^= key;
+        }
+        passOld = new String(textArray);
+        System.out.println(passOld);
+    }
+    }
