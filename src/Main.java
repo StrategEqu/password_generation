34cff77256289142a46c2fb69768d48f9b68c090
Дмитрий Когтев <129877863+StrategEqu@users.noreply.github.com>
2023-12-12 15:06:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 Generation generation = new Generation();
